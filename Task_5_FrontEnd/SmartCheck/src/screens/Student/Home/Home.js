32f02564a58a700543165110105fdd82c80e2ec0
TIWASTRANGE <ntiwa112@gmail.com>
2025-06-09 22:35:48 +0100

    Task_6 Document

--- Task_5_FrontEnd/SmartCheck/src/screens/Student/Home/Home.js
+++ Task_5_FrontEnd/SmartCheck/src/screens/Student/Home/Home.js
@@ -7,8 +7,8 @@ import {
   StyleSheet,
   TouchableOpacity,
 } from 'react-native';
-import Header from '../../components/Header';
-import Footer from '../../components/Footer';
+import Header from '../../../components/Header';
+import Footer from '../../../components/Footer';
 
 const Home = () => {
   const [activeTab, setActiveTab] = useState ('home');
