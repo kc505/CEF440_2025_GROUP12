99b0a89fa2b78ab131d9a5986465d806b827e626
unknown <zeufackkereine@gmail.com>
2025-06-09 20:51:12 +0100

    added course module and session management module

--- Task_5_FrontEnd/SmartCheck/android/app/.cxx/Debug/g03s1o14/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/hermes-engine/hermes-engineConfig.cmake
+++
@@ -1,9 +0,0 @@
-if(NOT TARGET hermes-engine::libhermes)
-add_library(hermes-engine::libhermes SHARED IMPORTED)
-set_target_properties(hermes-engine::libhermes PROPERTIES
-    IMPORTED_LOCATION "C:/Users/HP/.gradle/caches/8.13/transforms/7e279d2cbec2f2a8d23cf23fa13349f1/transformed/hermes-android-0.79.2-debug/prefab/modules/libhermes/libs/android.arm64-v8a/libhermes.so"
-    INTERFACE_INCLUDE_DIRECTORIES "C:/Users/HP/.gradle/caches/8.13/transforms/7e279d2cbec2f2a8d23cf23fa13349f1/transformed/hermes-android-0.79.2-debug/prefab/modules/libhermes/include"
-    INTERFACE_LINK_LIBRARIES ""
-)
-endif()
-
